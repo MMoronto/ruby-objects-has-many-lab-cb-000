6420a3e4be08e3637243154cd6a9a1cf1d8a6a38
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-03-21 02:44:32 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,20 +1,26 @@
 class Author  
-  attr_accessor :name
+  attr_accessor :name, :posts 
   
   def initialize(name)
     @name = name
+    @posts = []
   end
+  
+  def add_post(post)
+    post.author = self
+    @posts << post 
+    @@post_count += 1 
+  end 
   
   def add_post_by_title(title)
-    post = Post.new(title, self)
+    post = Post.new(title)  
+    post.author = self
+    @posts << post
+    @@post_count += 1 
   end
  
-  def posts
-    Post.all.select{|post| post.author == self }
-  end
-  
   def self.post_count
-    Post.all.length 
+    @@post_count  
   end 
   
 end 
