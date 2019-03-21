1b97031a5295a5a7baebbe18adf5f237465e78d5
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-03-21 02:35:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,20 @@
 class Artist 
-  attr_accessor :name
+  attr_accessor :name, :songs
+  @@songs = []
   
   def initialize(name)
     @name = name
+    @songs = []
   end 
   
   def add_song(song)
     @songs << song 
     song.artist = self
+    @@songs << song 
   end
   
-  def add_song_by_name(name)
-    song = Song.new(name, self)
+  def add_song_by_name(title)
+    song = Song.new(title, self)
   end
  
   def songs
